3cc3e833965ee5670128f2e9b83d6062be3d224e
Rebecca <rebecca.solomon446@gmail.com>
2023-01-17 15:56:59 +0000

    forms takes input and adds to to do list

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 // //Grabbing enter button from the form
-// const btn = document.querySelector("#enter");
+const btn = document.querySelector("#enter");
 
-// btn.addEventListener("click", registerClick);
+const toDoListForm = document.querySelector("#to-do-list-form")
 
-const toDoListForm = document.querySelector("to-do-list-form")
-
-const addToList = (item) => {
-    console.dir(item.target.toDoList.value);
+function addToList(item) {
+    // console.dir(item.target.toDoList.value);
     item.preventDefault();
+    console.log(item);
     const toDoItem = document.createElement("li");
-    toDoItem.textContent = item.target.toDoList.value;
-    const toDoListOne = document.querySelector("list");
-
+    toDoItem.textContent = item.target.toDo.value;
+    const toDoListOne = document.querySelector("#list");
     toDoListOne.appendChild(toDoItem);
 }
-toDoListForm.addEventListener('submit', addToList);
+
+toDoListForm.addEventListener("submit", addToList);
